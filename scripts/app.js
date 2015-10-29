043577661e89eb5927fe18ee4dae6d802e4d5c69
Katherine Ude <Katie.Ude@gmail.com>
2015-10-29 16:06:48 -0500

    hey hey hey

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// hey hey hey
+
+//
+window.onload = function() {
+  statusReport('hey HEY HEY!!!!!');
+  //alert('hey hey hey');
+}
+
+function statusReport(message) {
+  console.log(message);
+  return message;
+}
