183b62717b257440f91b20d548eed4cba0ae3904
Katherine Ude <Katie.Ude@gmail.com>
2015-10-29 16:22:47 -0500

    boilerplate fun

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,17 @@
 
 //
 window.onload = function() {
-  statusReport('hey HEY HEY!!!!!');
   //alert('hey hey hey');
+  statusReport('hey HEY HEY!!!!!');
+
+  var username = prompt('What is your name?');
+  var mission = prompt('What is your mission?');
+
+  var contentContainer =
+  document.getElementById('content');
+
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission
+
 }
 
 function statusReport(message) {
