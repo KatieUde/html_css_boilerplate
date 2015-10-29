ac728c3a3ab87117c7ed51d5098951877d573aa5
Katherine Ude <Katie.Ude@gmail.com>
2015-10-29 16:59:10 -0500

    Updating app.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,20 @@ window.onload = function() {
   //alert('hey hey hey');
   statusReport('hey HEY HEY!!!!!');
 
-  var username = prompt('What is your name?');
-  var mission = prompt('What is your mission?');
+
+  var dragonName = prompt('What would be your name if you were turned into a dragon?');
+  var power = prompt('What would your special power be? (hint: breathe fire)');
+  var region = prompt('Do you choose the mountainous region or the marshland?');
 
   var contentContainer =
   document.getElementById('content');
 
-  contentContainer.innerHTML = username + ' is on a mission to ' + mission
+  contentContainer.innerHTML = 'You have been transported to Vitrolla. Somehow as you were being\
+transported into the county, you were transformed into a dragon. Your current \
+name no longer fits you, so you pick a new ' + dragonName + '!!  In addition, you \
+discover that you have also gained magical abilites. In fact, you are particularly \
+skilled at ' + power + '!! As you fly over this new country, where do you want to \
+land? You pick the ' + region + '.'
 
 }
 
